a65233221ab662ae4b2c3d2313e03322990943a0
saya0118 <chemi0802@gmail.com>
2021-11-04 23:12:14 -0700

    did challenge#1 of 02

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,19 @@
-aaaa
+//Challenge #1
+const calcAverage = (score1, score2, score3) => (score1+score2+score3)/3;
+
+const aveDolphins = calcAverage(44,23,71);
+const aveKoalas = calcAverage(65,54,49);
+function checkWinner(aveDolphins, aveKoalas) {
+    if(aveDolphins>=aveKoalas*2){
+        console.log(`Dolphins win (${aveDolphins} vs. ${aveKoalas})`);
+    }if(aveKoalas>=aveDolphins*2){
+        console.log(`Koalas win (${aveDolphins}) vs. ${aveKoalas}`);
+    }else{
+        console.log("Neither win");
+    }
+    return checkWinner;
+}
+
+checkWinner(aveDolphins, aveKoalas);
+
+
