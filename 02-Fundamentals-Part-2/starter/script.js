4f2ae3845c8acb17e3e421e0e7bb94a8b3ed49a4
saya0118 <chemi0802@gmail.com>
2021-11-05 10:16:16 -0700

    challenge#3 of 02

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -18,16 +18,46 @@
 
 //Challenge #2------------------------------------------
 
-function calcTip(bill){
-    if(5<=bill<=300){
-        return bill*0.15;
-    }else{
-        return bill*0.2;
+// function calcTip(bill){
+//     if(5<=bill<=300){
+//         return bill*0.15;
+//     }else{
+//         return bill*0.2;
+//     }
+// }
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(bills, tips)
+// const total = [tips[0]+bills[0],tips[1]+bills[1],tips[2]+bills[2]];
+// console.log(total);
+
+//Challenge #3------------------------------------------
+const Mark = {
+    fullName:'Mark Miller',
+    mass:78,
+    height:1.69,
+    calcBMI :function(){
+        this.bmi = this.mass/this.height**2;
+        return this.bmi;
     }
 }
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills, tips)
-const total = [tips[0]+bills[0],tips[1]+bills[1],tips[2]+bills[2]];
-console.log(total);
+const John = {
+    fullName:'John Smith',
+    mass:92,
+    height:1.95,
+    calcBMI:function(){
+        this.bmi = this.mass/this.height**2;
+        return this.bmi;
+    }
+}
 
+Mark.calcBMI();
+John.calcBMI();
+
+console.log(Mark.bmi, John.bmi);
+
+if(Mark.bmi>John.bmi){
+    console.log(`${Mark.fullName}'s BMI(${Mark.bmi}) is higher than ${John.fullName}'s BMI(${John.bmi})`)
+}else if(John.bmi>Mark.bmi){
+    console.log(`${John.fullName}'s BMI(${John.bmi}) is higher than ${Mark.fullName}'s BMI(${Mark.bmi})`)
+}
